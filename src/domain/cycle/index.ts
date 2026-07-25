07ae4039096acd16fa7a8ba13abe67ad0b484efd
HemannB <brunohemann@gmail.com>
2026-07-25 00:14:45 -0300

    feat(domain): implement cycle calculator
    Signed-off-by: HemannB <brunohemann@gmail.com>

---
+++ src/domain/cycle/index.ts
@@ -0,0 +1,13 @@
+export {
+  CycleCalculator,
+  CycleDomainError,
+} from "./cycle-calculator";
+export type {
+  BeforeTreatmentDay,
+  BreakDay,
+  CycleBoundaries,
+  CycleConfiguration,
+  CycleDay,
+  DoseDay,
+  IsoDate,
+} from "./cycle-types";
